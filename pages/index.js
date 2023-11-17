1c799c0ed9b160207d8171126b636074c8658a96
vsathya94 <62723389+vsathya94@users.noreply.github.com>
2023-11-17 13:09:13 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -52,6 +52,14 @@ export default function Home() {
         </div>
       </main>
 
+              {
+  "extends": "next",
+  "rules": {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off"
+  }
+}
+              
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
